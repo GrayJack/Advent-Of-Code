4f21898953680a17fc5e6c0cd99a68115f88d67c
GrayJack <gr41.j4ck@gmail.com>
2019-12-01 03:22:26 -0300

    2019 - Day01

---
+++ 2019/day01/src/main.rs
@@ -0,0 +1,56 @@
+use std::io::Read;
+use std::{error::Error, fs::File};
+
+fn main() -> Result<(), Box<dyn Error>> {
+    let mut file = File::open("src/input.txt")?;
+    let mut input = String::new();
+    file.read_to_string(&mut input)?;
+
+    println!("Part 1: {}", part1(&input));
+    println!("Part 2: {}", part2(&input));
+
+    Ok(())
+}
+
+fn part1(str: &str) -> i128 {
+    str.split_whitespace().map(|e| e.parse::<i128>().unwrap()).map(|e| (e / 3) - 2).sum()
+}
+
+fn part2(str: &str) -> i128 {
+    str.split_whitespace()
+        .map(|e| e.parse::<i128>().unwrap())
+        .map(|e| (e / 3) - 2)
+        .map(|mut e| {
+            let mut ans = e;
+            while ans >= 0 {
+                ans = (ans / 3) - 2;
+                if ans > 0 {
+                    e += ans;
+                }
+            }
+            e
+        })
+        .sum()
+}
+
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_part1() {
+        assert_eq!(2, part1("12"));
+        assert_eq!(2, part1("14"));
+        assert_eq!(654, part1("1969"));
+        assert_eq!(33583, part1("100756"));
+    }
+
+    #[test]
+    fn test_part2() {
+        assert_eq!(2, part2("12"));
+        assert_eq!(2, part2("14"));
+        assert_eq!(966, part2("1969"));
+        assert_eq!(50346, part2("100756"));
+    }
+}
