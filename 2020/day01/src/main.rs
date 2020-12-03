6ddb434095ead59b41d3cc8b0cdd2894501d435f
GrayJack <gr41.j4ck@gmail.com>
2020-12-03 07:28:18 -0300

    2020: day 01: Rust

---
+++ 2020/day01/src/main.rs
@@ -0,0 +1,33 @@
+use itertools::Itertools;
+
+const INPUT: &str = include_str!("input.txt");
+
+type BoxResult<T> = Result<T, Box<dyn std::error::Error>>;
+
+fn main() -> BoxResult<()> {
+    let input: Result<Vec<_>, _> = INPUT.lines().map(|v| v.parse::<i64>()).collect();
+    let input = input?;
+
+
+    println!("Part1: {:?}", part1(&input)); // 158916
+    println!("Part2: {:?}", part2(&input)); // 165795564
+
+    Ok(())
+}
+
+fn part1(input: &[i64]) -> Option<i64> {
+    input
+        .iter()
+        .cartesian_product(input.iter().skip(1))
+        .find(|&(x, y)| (x + y) == 2020)
+        .map(|(x, y)| x * y)
+}
+
+fn part2(input: &[i64]) -> Option<i64> {
+    input
+        .iter()
+        .cartesian_product(input.iter().skip(1))
+        .cartesian_product(input.iter().skip(2))
+        .find(|&((x, y), z)| (x + y + z) == 2020)
+        .map(|((x, y), z)| x * y * z)
+}
